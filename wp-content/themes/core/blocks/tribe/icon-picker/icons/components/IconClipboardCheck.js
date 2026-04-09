277f07cedfc1da1168d1b3bc7e1c1fcdcc9a04d6
David Paul Ellenwood <davee@tri.be>
2026-04-09 10:38:06 -0500

    [MOOSE-382]: Run npm icon-picker to confirm it's working after upgrade.

--- wp-content/themes/core/blocks/tribe/icon-picker/icons/components/IconClipboardCheck.js
+++ wp-content/themes/core/blocks/tribe/icon-picker/icons/components/IconClipboardCheck.js
@@ -1,24 +1,25 @@
+import * as React from 'react';
 const SvgIconClipboardCheck = ( props ) => (
 	<svg
+		xmlns="http://www.w3.org/2000/svg"
 		width="1em"
 		height="1em"
-		viewBox="0 0 64 64"
 		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 64 64"
 		{ ...props }
 	>
 		<path
-			d="m24 38 6 6 12-12M38 8a6 6 0 1 0-12 0h-6v8h24V8h-6Z"
 			stroke="currentColor"
-			strokeWidth={ 2 }
-			strokeMiterlimit={ 10 }
 			strokeLinecap="square"
+			strokeMiterlimit={ 10 }
+			strokeWidth={ 2 }
+			d="m24 38 6 6 12-12M38 8a6 6 0 1 0-12 0h-6v8h24V8z"
 		/>
 		<path
-			d="M50 10h6v52H8V10h6"
 			stroke="currentColor"
-			strokeWidth={ 2 }
 			strokeMiterlimit={ 10 }
+			strokeWidth={ 2 }
+			d="M50 10h6v52H8V10h6"
 		/>
 	</svg>
 );
