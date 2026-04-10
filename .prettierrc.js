85377148c0690d54d48ed6169c319e77bfe0c74a
Saulo Padilha <saulo@tri.be>
2026-04-10 18:32:55 -0300

    Add dedicated Prettier override for .pcss files

--- .prettierrc.js
+++ .prettierrc.js
@@ -7,13 +7,19 @@
 const config = require( '@wordpress/prettier-config' );
 
 /**
- * Add .pcss to CSS file overrides.
+ * Add a dedicated .pcss override.
  *
  * The WordPress config only applies CSS formatting rules (singleQuote: false,
  * parenSpacing: false) to *.{css,sass,scss}. Without this, PostCSS files are
  * formatted with JavaScript rules, resulting in single quotes and unwanted
  * spacing like var( --spacer-40 ) instead of var(--spacer-40).
  */
-config.overrides[ 0 ].files = '*.{css,sass,scss,pcss}';
+config.overrides.push( {
+	files: '**/*.pcss',
+	options: {
+		singleQuote: false,
+		parenSpacing: false,
+	},
+} );
 
 module.exports = config;
